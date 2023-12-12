3088b6f0b6deb8e1423ca6e14e783ff109a1c248
Zisimopoulou <108530923+Zisimopoulou@users.noreply.github.com>
2023-12-12 22:27:16 +0200

    Update load_and_preprocess_pubmed_data and preprocess_text_with_line_numbers

--- data/data_loader.py
+++ data/data_loader.py
@@ -15,6 +15,12 @@ from tensorflow.keras import layers
 from tensorflow.keras.layers import TextVectorization
 
 def load_and_preprocess_pubmed_data(train_samples, val_samples, test_samples):    
+    data_dir = os.path.join(data_dir, "pubmed-rct", "PubMed_20k_RCT_numbers_replaced_with_at_sign")
+
+    train_samples = preprocess_text_with_line_numbers(os.path.join(data_dir, "train.txt"))
+    val_samples = preprocess_text_with_line_numbers(os.path.join(data_dir, "dev.txt"))
+    test_samples = preprocess_text_with_line_numbers(os.path.join(data_dir, "test.txt"))
+
     train_df = pd.DataFrame(train_samples)
     val_df = pd.DataFrame(val_samples)
     test_df = pd.DataFrame(test_samples)
@@ -34,32 +40,31 @@ def get_lines(filename):
         return f.readlines()
 
 def preprocess_text_with_line_numbers(filename):
-  input_lines = get_lines(filename) # get all lines from filename
-  abstract_lines = "" # create an empty abstract
-  abstract_samples = [] # create an empty list of abstracts
+  input_lines = get_lines(filename)  
+  abstract_lines = ""  
+  abstract_samples = [] 
   
-  # Loop through each line in target file
   for line in input_lines:
-    if line.startswith("###"): # check to see if line is an ID line
+    if line.startswith("###"):  
       abstract_id = line
-      abstract_lines = "" # reset abstract string
-    elif line.isspace(): # check to see if line is a new line
-      abstract_line_split = abstract_lines.splitlines() # split abstract into separate lines
+      abstract_lines = ""  
+    elif line.isspace():  
+      abstract_line_split = abstract_lines.splitlines()  
 
-      # Iterate through each line in abstract and count them at the same time
       for abstract_line_number, abstract_line in enumerate(abstract_line_split):
-        line_data = {} # create empty dict to store data from line
-        target_text_split = abstract_line.split("\t") # split target label from text
-        line_data["target"] = target_text_split[0] # get target label
-        line_data["text"] = target_text_split[1].lower() # get target text and lower it
-        line_data["line_number"] = abstract_line_number # what number line does the line appear in the abstract?
-        line_data["total_lines"] = len(abstract_line_split) - 1 # how many total lines are in the abstract? (start from 0)
-        abstract_samples.append(line_data) # add line data to abstract samples list
+        line_data = {}  
+        target_text_split = abstract_line.split("\t")  
+        line_data["target"] = target_text_split[0]  
+        line_data["text"] = target_text_split[1].lower()  
+        line_data["line_number"] = abstract_line_number  
+        line_data["total_lines"] = len(abstract_line_split) - 1  
+        abstract_samples.append(line_data)  
     
-    else: # if the above conditions aren't fulfilled, the line contains a labelled sentence
+    else:  
       abstract_lines += line
   
-    return abstract_samples
+  return abstract_samples
+
 
 def perform_one_hot_encoding(train_df, val_df, test_df, target_column="target"):
     one_hot_encoder = OneHotEncoder(sparse_output=False)
